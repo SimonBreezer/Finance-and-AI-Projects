a26d83126b752f06b44c4503785706b8a02f7b62
Simon <breezerizer@gmail.com>
2025-02-01 23:33:49 -0500

    Added additional variables impacting QA costs

--- game_qa_prediction/data_collection.py
+++ game_qa_prediction/data_collection.py
@@ -10,6 +10,10 @@ def generate_game_data(num_games):
             'levels': random.randint(5, 50),
             'characters': random.randint(20, 200),
             'items': random.randint(50, 500),
+            'hours_of_gameplay': random.randint(10, 100),  # Estimated hours of gameplay
+            'dialogue_lines': random.randint(500, 5000),  # Number of dialogue lines
+            'cutscenes': random.randint(5, 50),  # Number of cutscenes
+            'multiplayer': random.choice([True, False]),  # If the game has multiplayer
             'qa_cost': random.randint(10000, 500000)  # Simulated QA cost in dollars
         }
         games_data.append(game)
